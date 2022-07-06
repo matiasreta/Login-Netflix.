35218402dddbdf5f24aa4204041df1ef5bf2a988
Retamozo Matias <82097727+matiasreta@users.noreply.github.com>
2022-07-06 19:35:11 -0300

    update js

--- js/validacion.js
+++ js/validacion.js
@@ -1,1 +1,11 @@
-let nombre = document.getElementById("mail");
+
+    function onMouse(){
+        document.getElementById("mail").style.background = 'white';
+    };
+    function outMouse(){
+        document.getElementById("mail").style.background  = 'rgb(201, 201, 201)';
+    };
+
+    function validad(){
+        console.log("se envió")
+    }
