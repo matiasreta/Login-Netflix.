d4a8c8215fde229e8f6858c72948a8bb0f80a54d
Retamozo Matias <82097727+matiasreta@users.noreply.github.com>
2022-07-06 22:15:58 -0300

    update

--- js/validacion.js
+++ js/validacion.js
@@ -6,6 +6,19 @@
         document.getElementById("mail").style.background  = 'rgb(201, 201, 201)';
     };
 
-    function validad(){
-        console.log("se envió")
+    function validar(){
+        var error = false;
+        var mensaje="";
+        if(document.getElementById("mail").value==''){
+        error = true;
+        mensaje += "<p> el campo no puede estar vacio </p>";
+
+        }
+        if (error){
+            document.getElementById("mensaje").innerHTML = mensaje;
+        return false;
+        }
+        else{
+            return true;
+        }
     }
