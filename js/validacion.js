e8ae9b85b130528b4c0f67aa70ab5b370ba1a81d
Retamozo Matias <82097727+matiasreta@users.noreply.github.com>
2022-07-07 21:48:21 -0300

    update

--- js/validacion.js
+++ js/validacion.js
@@ -41,6 +41,32 @@
 
     }
 
+    function validarRecuperacion(){
+        var mensaje="";
+        if(document.getElementById("recuparar_mail").value==''){
+            mensaje += "<p class='alerta'> El campo Email no puede estar vacio </p>";
+            document.getElementById("error_mail_r").innerHTML = mensaje;
+            return false;
+        }
+        let correo =document.getElementById("recuparar_mail").value;
+        const expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+        let tieneLosCaracteresNecesarios = expReg.test(correo);
+        if(tieneLosCaracteresNecesarios == false){
+            mensaje += "<p class='alerta'> El campo Email no tiene los caracteres necesarios </p>";
+            document.getElementById("error_mail_r").innerHTML = mensaje;
+        return false;
+        }
+        
+        if(document.getElementById("recuparar_usuario").value==''){
+            mensaje += "<p class='alerta'> El campo Email no puede estar vacio </p>";
+            document.getElementById("error_usuario_r").innerHTML = mensaje;
+            return false;
+        }
+
+        else{
+            return false;
+        }
 
 
+    }
 
