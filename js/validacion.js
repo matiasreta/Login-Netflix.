c9ef7f6ca7cc7f0e97907f9f6a4c997d5a2b6a97
Retamozo Matias <82097727+matiasreta@users.noreply.github.com>
2022-07-07 21:31:00 -0300

    form v

--- js/validacion.js
+++ js/validacion.js
@@ -5,20 +5,42 @@
     function outMouse(){
         document.getElementById("mail").style.background  = 'rgb(201, 201, 201)';
     };
+    function onMousec(){
+        document.getElementById("contraseña").style.background = 'white';
+    };
+    function outMousec(){
+        document.getElementById("contraseña").style.background  = 'rgb(201, 201, 201)';
+    };
+
+    // voy a ir checkeando error por error si pasa va a preguntar
+    // el siguiente, con el objetivo de enviar un solo mensaje.
 
     function validar(){
-        var error = false;
         var mensaje="";
         if(document.getElementById("mail").value==''){
-        error = true;
-        mensaje += "<p> el campo no puede estar vacio </p>";
-
-        }
-        if (error){
-            document.getElementById("mensaje").innerHTML = mensaje;
+            mensaje += "<p class='alerta'> El campo Email no puede estar vacio </p>";
+            document.getElementById("error_mail").innerHTML = mensaje;
         return false;
         }
-        else{
-            return true;
+        let correo =document.getElementById("mail").value;
+        const expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+        let tieneLosCaracteresNecesarios = expReg.test(correo);
+        if(tieneLosCaracteresNecesarios == false){
+            mensaje += "<p class='alerta'> El campo Email no tiene los caracteres necesarios </p>";
+            document.getElementById("error_mail").innerHTML = mensaje;
+        return false;
         }
+        if(document.getElementById("password").value==''){
+            mensaje += "<p class='alerta'> El campo Constraseña no puede estar vacio </p>";
+            document.getElementById("error_contra").innerHTML = mensaje;
+            console.log("entro")
+            return false;
+        }else{
+            return false;
+        }
+
     }
+
+
+
+
